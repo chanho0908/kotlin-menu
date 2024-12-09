e96d8f5f9d3bd97307d61d7345b99fbc81002047
chanho0908 <uni070@naver.com>
2024-12-10 06:28:35 +0900

    feat : 메뉴 추천 useCase 추가 완료

---
+++ src/main/kotlin/menu/doamin/usecase/RecommendMenuUseCase.kt
@@ -0,0 +1,32 @@
+package menu.doamin.usecase
+
+import menu.doamin.model.MenuBoard.Companion.getMenuBoard
+import camp.nextstep.edu.missionutils.Randoms
+import menu.presentation.vm.model.UiEvent
+
+class RecommendMenuUseCase {
+    operator fun invoke(
+        category: Int,
+        ateMenu: List<String>,
+        notEatMenu: List<String>,
+    ): String {
+        val menu = getMenu(ateMenu, notEatMenu, category)
+        return menu
+    }
+
+    private fun getMenu(ateMenu: List<String>, notEatMenu: List<String>, category: Int): String {
+        val menuBoard = getMenuBoard(category)
+        var menu = ""
+        while (true) {
+            val newMenu = Randoms.shuffle(menuBoard)[0]
+            if (
+                ateMenu.contains(newMenu).not() &&
+                notEatMenu.contains(newMenu).not()
+            ) {
+                menu = newMenu
+                break
+            }
+        }
+        return menu
+    }
+}
